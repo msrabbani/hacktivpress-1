f1352880bc45df39b6b5edc991343011c8e1e64d
raynormw <tirtawiryaputra@yahoo.com>
2017-06-15 17:22:02 +0700

    Refactor

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -2,7 +2,7 @@
   <div class="post">
     <h2>New blog</h2><hr>
     <b-field label="Blog Title">
-      <b-input v-model="title" placeholder="Your blog title"></b-input>
+      <b-input v-model="title" placeholder="Your blog title" required></b-input>
     </b-field>
 
     <b-field label="Blog Category">
@@ -18,21 +18,25 @@
         minlength="10"
         maxlength="100"
         v-model="content"
-        placeholder="Content type here..">
+        placeholder="Content type here.." required>
       </b-input>
     </b-field>
     <button class="nav-item button is-primary is-outlined" @click="confirm"><i class="material-icons">exit_to_app</i>Submit</button>
 
     <sweet-modal icon="success" ref="alert_success">Success.<br>{{greet}}..</sweet-modal>
     <sweet-modal icon="warning" ref="alert_warning">
-    	Are you sure?<br><br>
-      <button class="button is-success" @click="signout">Yes</button>
+      Are you sure?<br><br>
+      <button class="button is-success" @click="post_blog">Yes</button>
       <button class="button is-danger" @click="close">No</button>
     </sweet-modal>
   </div>
+
 </template>
 
 <script>
+import { SweetModal, SweetModalTab } from 'sweet-modal-vue'
+import axios from 'axios'
+
 export default {
   name: 'post',
   props: ['title', 'category', 'content'],
@@ -43,6 +47,36 @@ export default {
       user_name: localStorage.getItem('user_name'),
       greet: ''
     }
+  },
+  methods: {
+    confirm () {
+      this.$refs.alert_warning.open()
+    },
+    close () {
+      this.$refs.alert_warning.close()
+    },
+    post_blog () {
+      let self = this
+      this.$refs.alert_warning.close()
+      axios.post('http://localhost:3000/articles', {title: this.title, content: this.content, category: this.category, user_name: this.user_name, author: this.user_id})
+      .then((res) => {
+        console.log(res.data)
+        this.greet = 'Thank you for posting..'
+        self.$refs.alert_success.open()
+        setTimeout(function () {
+          self.$router.push('/')
+        }, 2000)
+      })
+      .catch((err) => {
+        console.log('failed')
+        alert(err + 'register failed')
+        this.$refs.alert_warning.close()
+      })
+    }
+  },
+  components: {
+    SweetModal,
+    SweetModalTab
   }
 }
 </script>
